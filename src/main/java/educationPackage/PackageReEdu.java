2997dba56732c442e328052808a32088b2fa8555
SMK <coumarine@gmail.com>
2022-02-11 14:49:30 +0000

    Second Commit

--- src/main/java/educationPackage/PackageReEdu.java
+++ src/main/java/educationPackage/PackageReEdu.java
@@ -2,7 +2,7 @@ package educationPackage;
 
 public class PackageReEdu {
     public static void display(String param) {
-        System.out.println("The Parameter is : " + param);
+        System.out.println("The Parameter is :  " + param);
 
     }
 }
